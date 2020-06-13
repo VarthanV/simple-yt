d60ff00d95fcd395aab5e0ccb54179d2b5339a98
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-13 21:41:50 +0530

    Made Comment Parsing Work Perfectly 🔥 🔥 🔥 🔥

--- simpleyt/comment.py
+++ simpleyt/comment.py
@@ -3,7 +3,24 @@ class Comment(object):
         self.comment_dict  = comment_dict
         self.parse_comment()
     def parse_comment(self):
-        self.main_data = self.comment_dict.get('snippet').get('snippet')
+        self.main_data =self.comment_dict.get('snippet')
+        self.comment_data = self.main_data.get('topLevelComment').get('snippet')
     @property
-    def comment_text(self):
-        return self.main_data.get('textDisplay')       
+    def comment_text(self)->str:
+        """ The Comment made by the user """
+        return self.comment_data.get('textDisplay')  
+    @property
+    def author_name(self)->str:
+        """ The name of the author who made the comment """
+        return self.comment_data.get('authorDisplayName')
+    @property
+    def author_image_url(self)->str:
+        """ The Profile Image of the Author  """
+        return self.comment_data.get('authorProfileImageUrl')
+    @property
+    def like_count(self) -> int:
+        """ The Likes that the Comment got"""
+        return int(self.comment_data.get('likeCount'))    
+
+            
+    
--- simpleyt/video_comment.py
+++ simpleyt/video_comment.py
@@ -1,11 +1,24 @@
 from simpleyt.comment import Comment
 class VideoComment(object):
     def __init__(self,comments_obj:dict):
-        self.comments_obj =comments_obj
+        self._comments_obj =comments_obj
+        self._comments_list = []
         self.parse_comments()
+        
     def parse_comments(self):
-        self,main_data = ""
+        self.main_data =self._comments_obj.get('items')
+
+        for item in self.main_data:
+            self._comments_list.append(Comment(item))
+            
+
     @property
     def next_page_token(self) -> str :
         """  Next Page token """
-        return self.comments_obj.get('nextPageToken')    
+        return self._comments_obj.get('nextPageToken')    
+    @property
+    def response(self) ->dict:
+        return self._comments_obj
+    @property
+    def comments(self)  ->list:
+        return self._comments_list
