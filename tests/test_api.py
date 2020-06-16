69a8c91443faf75d4b805b8496c79c819a79eb09
Vishnu Varthan <vishnulatha006@gmail.com>
2020-06-16 14:48:20 +0530

    Added Test for get-channel method

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,7 @@
 import os
 import simpleyt
 from simpleyt import YoutubeVideo
+from simpleyt.youtube_channel import YouTubeChannel
 from dotenv import load_dotenv
 load_dotenv()
 """ Intialization """
@@ -9,7 +10,7 @@ client = simpleyt.YoutubeAPI(key)
 
 
 def test_video_method():
-    """  Test for the get video method"""
+    """  Test for the get_video method"""
     video = client.get_video('7M9hc_PC_Vg')
     assert isinstance(video, YoutubeVideo)
     assert video.title == 'Oxygen - Video Song | Kavan | Hiphop Tamizha | K V Anand | Vijay Sethupathi, Madonna Sebastian'
@@ -23,3 +24,16 @@ def test_video_method():
     assert isinstance(video.response,dict)
     assert isinstance(video.video_obj,dict)
     assert isinstance(video.duration,str)
+
+def test_channel_method():
+    """  Testing of the get_channel method  """
+    channel  = client.get_channel('UC-lHJZR3Gqxm24_Vd_AJ5Yw')
+    assert isinstance(channel,YouTubeChannel)
+    assert channel.name == 'PewDiePie'
+    assert channel.description == 'I make videos.'
+    assert channel.country == 'US'
+    assert isinstance(channel.subscribers_count,str)
+    assert isinstance(channel.topics,list)
+    assert channel.banner_image_url == 'https://yt3.ggpht.com/wuqXYCeCdttO0TcwBJR2yy0uJP2hPwTPdrDQpjD00t0Xd_81t6dYeLdVMR24ArD4kuIpWO4hWg=w1060-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj'
+    assert isinstance(channel.video_count,int)
+    assert isinstance(channel.view_count,int)
